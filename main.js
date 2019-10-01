63d0f5f7c7b17f5cff6b9661bbc046ddd198ddd8
Sung Paik <sung.paik@codeimmersives.com>
2019-10-01 19:27:19 -0400

    completed exercise

--- main.js
+++ main.js
@@ -1,20 +1,39 @@
-function isItLong() {
-
+function isItLong(str) {
+  if(str.length > 20) {
+    return 'That\'s a long string!';
+  }
 }
 
-function isItMedium() {
-
+function isItMedium(str) {
+  if(str.length >= 10 && str.length <=20) {
+    return 'That\'s a regular sized string!';
+  }
 }
 
-function isItShort() {
-
+function isItShort(str) {
+  if(str.length < 10) {
+    return 'That\'s a small string!';
+  }
 }
 
-function howLongIsMyString() {
-
+function howLongIsMyString(str) {
+  if(str.length > 20) {
+    return 'That\'s a long string!';
+  } else if(str.length >= 10 && str.length <=20) {
+    return 'That\'s a regular sized string!';
+  } else if(str.length < 10) {
+    return 'That\'s a small string!';
+  }
 }
 
-function instructorHeight() {
+function instructorHeight(str) {
+  if(str === 'Colin') {
+    return 62;
+  } else if(str === 'Mesuara') {
+    return 67;
+  } else {
+    return 'I don\'t know that instructor!';
+  }
 
 }
 
